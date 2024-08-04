fbc4470abd99c109b55474ad491bcb10b3576d03
Шамиль Гафиятуллин <samilgafiatullin@MacBook-Air-Samil.local>
2024-08-05 02:56:19 +0300

    Changing in database.py

--- Database/database.py
+++ Database/database.py
@@ -1,10 +1,8 @@
 import psycopg2
 from pgvector.psycopg2 import register_vector
-import os
 from psycopg2.extras import execute_values
 
 class Database():
-    # The database and the table in it must have already been created
     def __init__(self, database_name, table_name, user, password, host):
         self.table_name = table_name
         self.connection = psycopg2.connect(
@@ -17,7 +15,6 @@ class Database():
         self.cursor = self.connection.cursor()
         register_vector(self.connection)
 
-    # TODO: checking for duplicates
     def insert(self, data):
         execute_values(self.cursor, f"INSERT INTO {self.table_name} (name, embedding, song_fragment) VALUES %s", data)
 
