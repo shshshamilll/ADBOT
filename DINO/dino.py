0b07a3695fdd58b7287473923acd98f04163983b
Шамиль Гафиятуллин <samilgafiatullin@MacBook-Air-Samil.local>
2024-08-04 03:01:12 +0300

    Changing in sam.py and in dino.py

--- DINO/dino.py
+++ DINO/dino.py
@@ -6,15 +6,15 @@ class DINO():
         self.processor = AutoProcessor.from_pretrained("IDEA-Research/grounding-dino-tiny")
         self.model = AutoModelForZeroShotObjectDetection.from_pretrained("IDEA-Research/grounding-dino-tiny")
 
-    def get_box(self, image, text):
-        inputs = self.processor(images=image, text=text, return_tensors="pt")
+    def get_box(self, image_for_dino, object):
+        inputs = self.processor(images=image_for_dino, text=object, return_tensors="pt")
         with torch.no_grad():
             outputs = self.model(**inputs)
-        results = self.processor.post_process_grounded_object_detection(
+        result = self.processor.post_process_grounded_object_detection(
             outputs,
             inputs.input_ids,
             box_threshold=0.4,
             text_threshold=0.3,
-            target_sizes=[image.size[::-1]]
+            target_sizes=[image_for_dino.size[::-1]]
         )
-        return results[0]["boxes"]
+        return result[0]["boxes"].numpy()
